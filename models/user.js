7c41276a849a2c8ec69d8936e90623969524bb4f
virat122 <vikrantachaudhary96@gmail.com>
2025-07-12 15:07:38 +0530

    folder structure

---
+++ models/user.js
@@ -0,0 +1,25 @@
+// src/models/User.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+}, {
+  tableName: 'users',
+  timestamps: true,
+  underscored: true,
+});
+
+module.exports = User;
